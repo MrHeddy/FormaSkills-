f40b58a17fdb8b605852d9eb8abec3fea39eb88f
Tristan Tilmant <79102081+MrHeddy@users.noreply.github.com>
2024-03-11 23:56:46 +0100

    First make

---
+++ formaskills/app/main/page.tsx
@@ -0,0 +1,18 @@
+import  { Menu }  from "@/components/Menu";
+export default function Home() {
+  return ( 
+
+<body>
+<div id="home">
+<Menu/>
+
+  </div>
+<div id ="Titre">
+  <h1>Home</h1>
+  <h1>me</h1>
+  <h1>o</h1>
+  <h1>H</h1>
+   </div>
+</body>
+  );
+}
---
+++ formaskills/components/Menu.tsx
@@ -0,0 +1,12 @@
+export const Menu = () => {
+    return (
+        <div id="menu">
+            <div id="A">Acceuil</div>
+            <div id="S">Services</div>
+            <div id="P">Portfolio</div>
+            <div id="E">Equipe</div>
+            <div id="C">Contact</div>
+
+        </div>
+    );
+    }

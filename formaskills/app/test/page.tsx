fd47013c7f70a93d4a6c0764747722ffe1e0daf3
Tristan Tilmant <Tristankopat@gmail.com>
2024-03-14 15:28:10 +0100

    Maj

---
+++ formaskills/app/test/page.tsx
@@ -0,0 +1,15 @@
+import { Menu } from "@/components/Menu";
+export default function Test() {
+    return (
+        <div className="container">
+            <div>
+                <Menu />
+            </div>
+
+            <div id="menu">
+                Youpi
+            </div>
+        </div>
+
+    );
+}
--- formaskills/components/Menu.tsx
+++ formaskills/components/Menu.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export const Menu = () => {
     return (
         <div id="menu">
-            <div id="A">Acceuil</div>
-            <div id="S">Services</div>
+            {/* Youpi */}
+            <div id="A"><Link href="/">Accueil</Link></div>
+            <div id="S"><Link href="/test">Services</Link></div>
             <div id="P">Portfolio</div>
             <div id="E">Equipe</div>
             <div id="C">Contact</div>
